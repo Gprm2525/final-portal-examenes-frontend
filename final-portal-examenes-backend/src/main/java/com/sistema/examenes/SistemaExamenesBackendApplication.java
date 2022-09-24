4b4da9dd60611bb3b4e991f488daf955bca26ecc
Giorgio Ruitón Mauricci <74784411+Gprm2525@users.noreply.github.com>
2022-09-24 11:18:36 -0500

    Add files via upload

---
+++ final-portal-examenes-backend/src/main/java/com/sistema/examenes/SistemaExamenesBackendApplication.java
@@ -0,0 +1,33 @@
+package com.sistema.examenes;
+
+import com.sistema.examenes.excepciones.UsuarioFoundException;
+import com.sistema.examenes.modelo.Rol;
+import com.sistema.examenes.modelo.Usuario;
+import com.sistema.examenes.modelo.UsuarioRol;
+import com.sistema.examenes.servicios.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@SpringBootApplication
+public class SistemaExamenesBackendApplication implements CommandLineRunner {
+
+	@Autowired
+	private UsuarioService usuarioService;
+
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaExamenesBackendApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	}
+}
